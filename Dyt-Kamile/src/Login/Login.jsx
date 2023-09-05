664bc9dba422c7568430e0bda1ff1663e51e6df4
Mehmet Çimen <mehmet.cimen@ogr.gidatarim.edu.tr>
2023-09-05 18:50:24 +0300

    ilk sayfa yazı ve button
    ilk sayfa için yazı ve button ayarlandı

--- Dyt-Kamile/src/Login/Login.jsx
+++ Dyt-Kamile/src/Login/Login.jsx
@@ -7,11 +7,21 @@ export const Login = () => {
          <div>
             <div className='desingLogin'><svg  xmlns="http://www.w3.org/2000/svg"  height="752" viewBox="0 0 1440 752" fill="none">
   <path d="M0 -37H1440V689L903.04 741.786C769.684 754.895 635.348 754.57 502.058 740.814L0 689V-37Z" fill="#F2DFF7" fill-opacity="0.87"/>
-</svg></div>
+</svg>
+</div>
             <div className='desingLogin'><svg  xmlns="http://www.w3.org/2000/svg"  height="752" viewBox="0 0 1440 752" fill="none">
   <path d="M-89 -59H1482V667.455L956.5 704.5L780.914 730.435C508.249 770.71 230.202 754.192 -35.7763 681.917L-89 667.455V-59Z" fill="#E5BEEB" fill-opacity="0.3"/>
-</svg></div>
-  
+</svg>
+</div>
+<div className='conteiner'>
+   <div className='text-conteiner'>
+    <p className='login-content'><strong className='login-bold'>Sağlıklı</strong> yaşam düşündüğünüzden daha kolay</p>
+  </div>
+  <div>
+    <button className='login-button'>Giriş Yap / Üye Ol</button>
+  </div>
+</div>
+ 
 
     </div>
     <About></About>
