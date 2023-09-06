ecb03354210071f0d686828e05c54b3de79eb6b8
Mehmet Çimen <mehmet.cimen@ogr.gidatarim.edu.tr>
2023-09-06 13:51:25 +0300

    about için resim ve hakkımda eklendi

--- Dyt-Kamile/src/Login/About.jsx
+++ Dyt-Kamile/src/Login/About.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import diyet from '../total/images/en-iyi-diyet-yemeklerijpg-f-zg-k-11.png'
 export const About = () => {
   return (
-    <div id='about'>About</div>
+    <div id='about'>
+      <div>
+       <div className='diet-food'>
+        <img src={diyet} alt="diyet" />
+      </div>
+      <div className='about-conteiner'>
+        <p className='about-text'>HAKKIMDA</p>
+      </div>
+    </div>
+     
+    </div>
   )
 }
--- Dyt-Kamile/src/Login/Login.jsx
+++ Dyt-Kamile/src/Login/Login.jsx
@@ -6,11 +6,11 @@ export const Login = () => {
     <div className='login'>
          <div>
             <div className='desingLogin'><svg  xmlns="http://www.w3.org/2000/svg"  height="1440" viewBox="0 0 1440 752" fill="none">
-  <path d="M0 -37H1440V689L903.04 741.786C769.684 754.895 635.348 754.57 502.058 740.814L0 689V-37Z" fill="#F2DFF7" fill-opacity="0.87"/>
+  <path d="M0 -37H1440V689L903.04 741.786C769.684 754.895 635.348 754.57 502.058 740.814L0 689V-37Z" fill="#F2DFF7" fillOpacity="0.87"/>
 </svg>
 </div>
             <div className='desingLogin'><svg  xmlns="http://www.w3.org/2000/svg"  height="1440" viewBox="0 0 1440 752" fill="none">
-  <path d="M-89 -59H1482V667.455L956.5 704.5L780.914 730.435C508.249 770.71 230.202 754.192 -35.7763 681.917L-89 667.455V-59Z" fill="#E5BEEB" fill-opacity="0.3"/>
+  <path d="M-89 -59H1482V667.455L956.5 704.5L780.914 730.435C508.249 770.71 230.202 754.192 -35.7763 681.917L-89 667.455V-59Z" fill="#E5BEEB" fillOpacity="0.3"/>
 </svg>
 </div>
 <div className='conteiner'>
