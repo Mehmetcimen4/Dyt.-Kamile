4cb7e8f9ee054208e4a9a338279347e0786d20ac
Mehmet Çimen <mehmet.cimen@ogr.gidatarim.edu.tr>
2023-09-09 22:10:41 +0300

    hata gidermeleri

--- Dyt-Kamile/src/Login/About.jsx
+++ Dyt-Kamile/src/Login/About.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
-import diyet from '../total/images/en-iyi-diyet-yemeklerijpg-f-zg-k-11.png'
+import regt from '../total/images/rectangle-415.png'
 export const About = () => {
   return (
     <div id='about'>
       <div>
        <div className='diet-food'>
-        <img src={diyet} alt="diyet" />
-      </div>
-      <div className='about-conteiner'>
+          <div className='about-conteiner'>
         <p className='about-text'>HAKKIMDA</p>
+      </div>
+      </div>
+    
+      <div>
+        <img src={regt} alt="" />
+      </div>
+      <div className='about-div'>
+        <h3 className='about-div-title'>Merhabalar Ben Kamile</h3>
+        <p className='about-div-text'></p>
       </div>
     </div>
      
     </div>
   )
 }
+export default About;
--- Dyt-Kamile/src/Login/Contact.jsx
+++ Dyt-Kamile/src/Login/Contact.jsx
@@ -5,3 +5,4 @@ export const Contact = () => {
     <div id='contact'>Contact</div>
   )
 }
+export default Contact;
