5c9310d1861254ff1e66d902960e66635704477e
Mehmet Çimen <mehmet.cimen@ogr.gidatarim.edu.tr>
2023-09-12 22:20:19 +0300

    who.jsx eklemeleri

--- Dyt-Kamile/src/Login/Who.jsx
+++ Dyt-Kamile/src/Login/Who.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import circles from '../total/images/circles.png'
 export const Who = () => {
   return (
     <div className='who-div'>
     <div className='who-div-image1'>
-       
+      <div className='who-div-text'>
+        <div className='who-div-regt'></div>
+        <p className='who-text-w'>DİYETİSY</p><p className='who-text-p'>EN DENİR</p>
+      </div>
+       <div className='who-circles'>
+        <img src={circles} alt="" />
+       </div>
     </div>
     <div className='who-div-image2'>
         
