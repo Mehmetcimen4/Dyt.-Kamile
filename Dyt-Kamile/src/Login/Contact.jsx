aee5b2bba936a0c821edd3d0aa755cea4b22632f
Mehmet Çimen <mehmet.cimen@ogr.gidatarim.edu.tr>
2023-09-16 00:25:02 +0300

    contact.jsx büyük çoğunluğu tamamlandı

--- Dyt-Kamile/src/Login/Contact.jsx
+++ Dyt-Kamile/src/Login/Contact.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import face from '../total/images/group.png'
+import insta from '../total/images/group2.png'
+import wp from '../total/images/group3.png'
+import linkedin from'../total/images/group4.png'
 
 export const Contact = () => {
   return (
@@ -8,21 +12,46 @@ export const Contact = () => {
       </div>
       <div className='contact-form'>
         <form action="">
-         <label className='contact-form-label' htmlFor="">Adınız</label>
-        <input type="text" placeholder="" />
-        <label className='contact-form-label' htmlFor="">Soyadınız</label>
-        <input type="text" />
-        <label className='contact-form-label' htmlFor="">Email</label>
-        <input type="email" placeholder=''/>
-        <label className='contact-form-label' htmlFor="">Hangi hizmetle ilgileniyorsunuz</label>
-        <select>
+          <div className='contact-gap'> 
+             <label className='contact-form-label' htmlFor="">Adınız</label>
+        <input className='contact-input' type="text" placeholder="" />
+        </div>
+          <div className='contact-gap'>
+            <label className='contact-form-label' htmlFor="">Soyadınız</label>
+        <input className='contact-input' type="text" />
+        </div>
+          <div className='contact-gap'>
+             <label className='contact-form-label' htmlFor="">Email</label>
+        <input className='contact-input' type="email" placeholder=''/>
+        </div>
+          <div className='contact-gap'>
+            <label className='contact-form-label' htmlFor="">Hangi hizmetle ilgileniyorsunuz</label>
+        <select className='contact-input'>
           <option value="Herhangi birini şeçin">Herhangi birini şeçin</option>
           <option value=""></option>
         </select>
-        <label className='contact-form-label' htmlFor="">Mesaj</label>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
-      </form>
+        </div>
+          <div className='contact-gap'>
+             <label className='contact-form-label' htmlFor="">Mesaj</label>
+        <textarea className='contact-textarea' name="" id="" cols="30" rows="10"></textarea>
+      
+          </div>
+       </form>
        
+      </div>
+      <div className='contact-icon-div'>
+      <span>
+        <img src={face} alt="" />
+        </span>
+      <span>
+      <img src={insta} alt="" />
+      </span>
+      <span>
+      <img src={wp} alt="" />
+      </span>
+      <span>
+      <img src={linkedin} alt="" />
+      </span>
       </div>
     </div>
   )
--- Dyt-Kamile/src/Login/Who.jsx
+++ Dyt-Kamile/src/Login/Who.jsx
@@ -3,12 +3,14 @@ import circles from '../total/images/circles.png'
 export const Who = () => {
   return (
     <div className='who-div'>
-    <div className='who-div-image1'>
-      <div className='who-div-text'>
+    <div className='who-div-image1'> 
+    <p className='who-text-p who-top '>KİMLERE</p>
+      <div className='who-div-text'> 
+     
         <div className='who-div-regt'>
-           <p className='who-text-w'>DİYETİSYEN</p>
+           <p className='who-text-w'>DİYETİSY</p>
         </div>
-       <p className='who-text-p'>KİMLERE DENİR</p>
+       <p className='who-text-p'>EN DENİR</p>
       </div>
        <div className='who-circles'>
         <img src={circles} alt="" />
